cdf1e30dbc717c833d433c833b77a8d92b2908ff
litao <litaowork@aliyun.com>
2017-01-12 08:57:01 +0800

    实现点击图标生成 md 文件到默认下载目录

--- extract.js
+++ extract.js
@@ -6,28 +6,26 @@ function getCurrentTabUrl(callback) {
     currentWindow: true
   };
 
-  callback('sdfsdfsdf');
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
 
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   // chrome.tabs.query invokes the callback with a list of tabs that match the
-  //   // query. When the popup is opened, there is certainly a window and at least
-  //   // one tab, so we can safely assume that |tabs| is a non-empty array.
-  //   // A window can only have one active tab at a time, so the array consists of
-  //   // exactly one tab.
-  //   var tab = tabs[0];
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+    var title = tab.title;
 
-  //   // A tab is a plain object that provides information about the tab.
-  //   // See https://developer.chrome.com/extensions/tabs#type-Tab
-  //   var url = tab.url;
-
-  //   // tab.url is only available if the "activeTab" permission is declared.
-  //   // If you want to see the URL of other tabs (e.g. after removing active:true
-  //   // from |queryInfo|), then the "tabs" permission is required to see their
-  //   // "url" properties.
-  //   console.assert(typeof url == 'string', 'tab.url should be a string');
-  //   renderStatus(url);
-  //   callback(url);
-  // });
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(title, url);
+  });
 
   // Most methods of the Chrome extension APIs are asynchronous. This means that
   // you CANNOT do something like this:
@@ -55,10 +53,12 @@ function downloadFile(fileName, content){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    url = url || 'litao';
+  getCurrentTabUrl(function(title, url) {
     renderStatus('Performing create file for ' + url + ' ...');
+    // 内容
+    var content = '[' + title + ']' + '(' + url + ')';
     // 先实现生成文件
-    downloadFile('test.md', url);
+    title = title.replace(' ', '');
+    downloadFile(title + '.md', content);
   });
 });
